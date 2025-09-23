3b8299a25ff3be34421eef3b80c72ed023cf8fab
Iqbal Azam <azamiqbal09876@gmail.com>
2025-09-23 19:09:58 +0530

    Time: 33 ms (17.29%) | Memory: 45.2 MB (30.31%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -1,10 +1,10 @@
 class Solution {
     int dp[][];
-    public int recur(int idx, int target, int[] coins){
+    public int recur(int idx, int target, int coins[]){
         if(target == 0) return 0;
-        if(idx >= coins.length || target < 0) return 100000;
+        if (idx >= coins.length || target < 0) return 100000;
         if(dp[idx][target] != -1) return dp[idx][target];
-        int take = 1+ recur(idx, target-coins[idx], coins);
+        int take = 1 + recur(idx, target-coins[idx], coins);
         int skip = recur(idx+1, target, coins);
         int ans = Math.min(take, skip);
         dp[idx][target] = ans;
@@ -12,14 +12,13 @@ class Solution {
     }
     public int coinChange(int[] coins, int amount) {
         int n = coins.length;
-        if(amount == 0) return 0;
-        
         dp = new int[n][amount+1];
         for(int i = 0; i < n; i++){
             for(int j = 0; j < amount+1; j++)
             dp[i][j] = -1;
         }
         int ans = recur(0, amount, coins);
-        return (ans >= 100000) ? -1 : ans;
+        if(ans == 100000) return -1;
+        return ans;
     }
 }
