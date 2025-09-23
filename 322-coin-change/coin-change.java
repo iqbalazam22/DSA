0cb9288dd33be9920fadd23db38bb0431263d365
Iqbal Azam <azamiqbal09876@gmail.com>
2025-09-23 19:30:44 +0530

    Time: 22 ms (35.73%) | Memory: 44.8 MB (48.15%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -1,23 +1,24 @@
 class Solution {
-    int dp[][];
-    public int recur(int idx, int target, int coins[]){
+    int dp[];
+    public int recur(int target, int coins[]){
         if(target == 0) return 0;
-        if (idx >= coins.length || target < 0) return 100000;
-        if(dp[idx][target] != -1) return dp[idx][target];
-        int take = 1 + recur(idx, target-coins[idx], coins);
-        int skip = recur(idx+1, target, coins);
-        int ans = Math.min(take, skip);
-        dp[idx][target] = ans;
+        // if (idx >= coins.length || target < 0) return 100000;
+        if(dp[target] != -1) return dp[target];
+        int ans = 100000;
+        for(int i = 0; i < coins.length; i++){
+            if(target >= coins[i]){
+                int take = 1 + recur(target-coins[i], coins);
+                ans = Math.min(ans, take);
+            }
+        }
+        dp[target] = ans;
         return ans;
     }
     public int coinChange(int[] coins, int amount) {
-        int n = coins.length;
-        dp = new int[n][amount+1];
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < amount+1; j++)
-            dp[i][j] = -1;
-        }
-        int ans = recur(0, amount, coins);
+        dp = new int[amount+1];
+        for(int j = 0; j < amount+1; j++)
+        dp[j] = -1;
+        int ans = recur(amount, coins);
         if(ans == 100000) return -1;
         return ans;
     }
