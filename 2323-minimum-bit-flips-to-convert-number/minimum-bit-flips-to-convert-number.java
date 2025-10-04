7e4c56081308ffd6b6a9eb3b8c533ad96487a7a5
Iqbal Azam <azamiqbal09876@gmail.com>
2025-10-04 23:00:48 +0530

    Time: 0 ms (100.00%) | Memory: 40.7 MB (36.44%) - LeetSync

--- 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.java
+++ 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.java
@@ -1,9 +1,14 @@
 class Solution {
     public int minBitFlips(int start, int goal) {
-        int counter = 0;
-        for(int i = 0; i < 32; i++){
-            if((start & (1 << i)) != (goal & (1 << i))) counter++;
+        int xor = start ^ goal;  // it will tell how many bits are different as 1^0 = 1 and 1^1 or 0^0 = 0
+
+        int count = 0;
+        while(xor != 0){
+            xor = xor & (xor - 1);    
+            count++;
         }
-        return counter;
+        return count;
     }
 }
+// this always removes right set bit, as 1101 & 1100 it will remove last 1, them it will be 1100 & 1011 again removes last then 1000 & 0000 => 0000, 
+// and counter will be updated by 1 always until it is 0
