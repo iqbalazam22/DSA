2a85a69bb8b242ce79cdbf425d248fe1f90e4c21
Iqbal Azam <azamiqbal09876@gmail.com>
2025-10-04 23:19:33 +0530

    Time: 38 ms (69.89%) | Memory: 44.3 MB (58.65%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -2,20 +2,21 @@ class Solution {
     public int lengthOfLIS(int[] nums) {
         int n = nums.length;
         int dp[] = new int[n];
+        dp[0] = 1;  // always +1 in length of answer while counting answers using dp
 
-        dp[0] = 1;
-        int finalAns = 0;
-        int currentAns = 1;
-        for(int i = 0; i < n; i++){
-            currentAns = 1;
+        int currAns = 1;   // for answer currently
+        int finAns = 0;    // for maximum answer
+
+        for(int i = 0; i < nums.length; i++){
+            currAns = 1;        // reset to 1 each time loop runs
             for(int j = 0; j < i; j++){
                 if(nums[i] > nums[j]){
-                    currentAns = Math.max(currentAns, 1 + dp[j]);
+                    currAns = Math.max(currAns, 1 + dp[j]);
                 }
             }
-            dp[i] = currentAns;
-            finalAns = Math.max(finalAns, currentAns);
+            dp[i] = currAns;
+            finAns = Math.max(currAns, finAns);
         }
-        return finalAns;
+        return finAns;
     }
 }
