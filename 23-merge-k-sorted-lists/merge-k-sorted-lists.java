9b2df6b3b5e448afcf4b8fbcee09d4b9db122369
Iqbal Azam <azamiqbal09876@gmail.com>
2025-10-06 15:26:54 +0530

    Time: 141 ms (5.01%) | Memory: 44.5 MB (66.07%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.java
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.java
@@ -10,18 +10,20 @@
  */
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
-        int min = Integer.MAX_VALUE;
+        int min = (int)(1e9);
         int idx = -1;
+
         for(int i = 0; i < lists.length; i++){
             if(lists[i] != null && lists[i].val < min){
-                idx = i;
                 min = lists[i].val;
+                idx = i;
             }
         }
         if(idx == -1) return null;
-            ListNode head = lists[idx];
-            lists[idx] = lists[idx].next;
-            head.next = mergeKLists(lists);
-            return head;
+        
+        ListNode head = lists[idx];
+        lists[idx] = lists[idx].next;
+        head.next = mergeKLists(lists);
+        return head;
     }
 }
