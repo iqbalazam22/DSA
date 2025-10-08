5282449c8d081c7d70b54d88278a5e53052d9ee7
Iqbal Azam <azamiqbal09876@gmail.com>
2025-10-08 21:42:55 +0530

    Time: 33 ms (31.46%) | Memory: 51.2 MB (97.15%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,27 +1,32 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> list = new ArrayList<>();
+
         Arrays.sort(nums);
-       List<List<Integer>> list = new ArrayList<>();
-       int i =0;
-        while(i<nums.length){
-            int currentElement = nums[i];
-            int target = -currentElement;
-            int a=i+1, b=nums.length-1;
-                while(a<b){
-                    int sum = nums[a]+nums[b];
-                    if(sum == target){
-                    list.add(Arrays.asList(nums[i],nums[a],nums[b]));
-                    int currentE = nums[a];
-                    while(a < nums.length && currentE == nums[a])a++;
-                    currentE = nums[b];
-                    while(b > 0 && currentE == nums[b])b--;
-                    }
-                    else if(sum > target)b--;
-                    else a++;
-                
-                }    
-                   
-            while(i < nums.length && currentElement==nums[i]) i++;
+        int n = nums.length;
+
+        for(int i = 0; i < n-2; i++){
+            if(i > 0 && nums[i] == nums[i-1]) continue;
+
+            int firstInt = nums[i];
+            int target = -firstInt;
+            
+            int start = i+1; 
+            int end = n-1;
+            
+            while(start < end){
+                if(nums[start] + nums[end] == target){
+                    ArrayList<Integer> ans = new ArrayList<>();
+                    list.add(Arrays.asList(firstInt, nums[start], nums[end]));
+                    while (start < end && nums[start] == nums[start + 1]) start++;
+                    while (start < end && nums[end] == nums[end - 1]) end--;
+                    start++;
+                    end--;
+                }
+                if(start < end && nums[start] + nums[end] > target) end--;
+                else if(start < end && nums[start] + nums[end] < target) start++;
+               
+            }
         }
         return list;
     }
