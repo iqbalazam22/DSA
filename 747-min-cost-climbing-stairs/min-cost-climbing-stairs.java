8b9acc8b1e0b024e63fb4489cb5ca3dcd0a2ee77
Iqbal Azam <azamiqbal09876@gmail.com>
2025-10-10 19:36:23 +0530

    Time: 1 ms (37.63%) | Memory: 43.1 MB (64.96%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.java
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.java
@@ -1,26 +1,23 @@
 class Solution {
     int dp[];
-    public int recur(int i, int cost[]){
-        if(i >= cost.length){
-            return 0;
-        }
-        if(dp[i] != -1){
-            return dp[i];
-        }
-        int op1 = (int)(1e9);
-        int op2 = (int)(1e9);
+    public int recur(int idx, int[] cost){
+        if(idx >= cost.length) return 0;
 
-        op1 = cost[i] + recur(i+1, cost);
-        op2 = cost[i] + recur(i+2, cost);
+        if(dp[idx] != -1) return dp[idx];
+
+        int op1 = 1000000;
+        int op2 = 1000000;
+
+        op1 = cost[idx] + recur(idx+1, cost);
+        op2 = cost[idx] + recur(idx+2, cost);
 
         int ans = Math.min(op1, op2);
-        dp[i] = ans;
+        dp[idx] = ans;
         return ans;
     }
     public int minCostClimbingStairs(int[] cost) {
-        int n = cost.length;
-        dp = new int[n+1];
-        for(int i = 0; i <= n; i++) dp[i] = -1;
-        return Math.min(recur(0, cost), recur(1, cost));
+        dp = new int[cost.length];
+        for(int i = 0; i < cost.length; i++) dp[i] = -1;
+        return Math.min(recur(1, cost), recur(0, cost));
     }
 }
