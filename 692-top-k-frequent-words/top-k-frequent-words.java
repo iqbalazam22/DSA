19dc64d6fb2593ce4459ad995a67788329e131b9
Iqbal Azam <azamiqbal09876@gmail.com>
2025-10-14 12:53:11 +0530

    Time: 7 ms (66.33%) | Memory: 44.9 MB (37.74%) - LeetSync

--- 692-top-k-frequent-words/top-k-frequent-words.java
+++ 692-top-k-frequent-words/top-k-frequent-words.java
@@ -4,19 +4,22 @@ class Solution {
         for(int i = 0; i < words.length; i++){
             map.put(words[i], map.getOrDefault(words[i], 0)+1);
         }
-        PriorityQueue<String> q = new PriorityQueue<>((a, b)->{
-            int fa = map.get(a), fb = map.get(b);
-            if (fa == fb) return b.compareTo(a); // lexicographically larger last
-            return fa - fb; // lower freq first
+        PriorityQueue<String> heap = new PriorityQueue<>((a, b)->{
+            int fA = map.get(a);
+            int fB = map.get(b);
+
+            if(fA == fB) return b.compareTo(a);
+            return fA - fB;
         });
         for(String word : map.keySet()){
-            q.add(word);
-            if(q.size() > k) q.remove();
+            heap.add(word);
+
+            if(heap.size() > k) heap.remove();
         }
         List<String> list = new ArrayList<>();
-        
-        while(!q.isEmpty()){
-            list.add(q.remove());
+
+        while(!heap.isEmpty()){
+            list.add(heap.remove());
         }
         Collections.reverse(list);
         return list;
